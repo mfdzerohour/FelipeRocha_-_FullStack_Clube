cb93058bc19c060d53cbd8d11ccde5fdc054e699
mfdzerohour <mfdzerohour@hotmail.com>
2025-05-07 15:28:44 -0400

    Ajustando componentes

--- CursoReactParaCompletos2024/src/App.jsx
+++ CursoReactParaCompletos2024/src/App.jsx
@@ -1,18 +1,37 @@
+import AddTask from './components/AddTask/AddTask.jsx';
+import Tasks from './components/Tasks/Tasks.jsx';
 import { useState } from 'react';
 
 function App(){
-  //State (Estado)
-  const [message, setMessage] = useState('Olá mundo!')
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Estudar programação',
+      description: "Estudar programação para se tornar um desenvolvedor fullstack",
+      isCompleted: false,
+    }, {
+      id: 2,
+      title: 'Estudar inglês',
+      description: "Estudar inglês para se tornar fluente",
+      isCompleted: false,
+    }, {
+      id: 3,
+      title: 'Estudar matemática',
+      description: "Estudar matemática para se tornar um desenvolvedor fullstack",
+      isCompleted: false    
+    },
+  ]);
 
-  // let message = 'Olá Mundo!'
   return (
-    <>
-      <h1>{message}</h1>
-      <button onClick={() => 
-                            setMessage('Olá, fui clicado!')}
-        >Mudar mensagem!
-      </button>
-    </>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks />
+      </div>
+    </div>
   )
 }
 
