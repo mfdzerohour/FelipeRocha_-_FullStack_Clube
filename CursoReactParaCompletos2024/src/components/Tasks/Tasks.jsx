d90cdd6e22f70ea695936a2e98c7b968beda9576
mfdzerohour <mfdzerohour@hotmail.com>
2025-05-08 17:06:25 -0400

    Mexendo na atuailização da página detalhes

--- CursoReactParaCompletos2024/src/components/Tasks/Tasks.jsx
+++ CursoReactParaCompletos2024/src/components/Tasks/Tasks.jsx
@@ -1,12 +1,29 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+// import { useNavigate as userNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 function Tasks({ tasks, onTaskClick, onTaskDeleteClick }) {
+    const navigate = useNavigate();
+
+    function onSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set("title", task.title);
+        query.set("description", task.description);
+
+        navigate(`/task?${query.toString()}`);
+    }
+    function handleTaskClick(taskId) {
+        onTaskClick(taskId);
+    }
+
+
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow ">
             {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
                     <button
-                        onClick={() => onTaskClick(task.id)}
+                        onClick={() => handleTaskClick(task.id)}
                         className="bg-slate-400 text-left w-full text-white p-2 rounded-md"
                     >
                         <span className={task.isCompleted ? "line-through" : ""}>
@@ -14,7 +31,9 @@ function Tasks({ tasks, onTaskClick, onTaskDeleteClick }) {
                         </span>
                         {task.isCompleted ? " ✅ COMPLETED" : " ⛔ INCOMPLETE"}
                     </button>
-                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                    <button
+                        onClick={() => onSeeDetailsClick(task)}
+                        className="bg-slate-400 p-2 rounded-md text-white">
                         <ChevronRightIcon />
                     </button>
                     <button
--- CursoReactParaCompletos2024/src/main.jsx
+++ CursoReactParaCompletos2024/src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import TaskPage from '../src/pages/TaskPages.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: 'task',
+    element: <TaskPage />,
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
