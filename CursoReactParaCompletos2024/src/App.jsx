9bbe155a71eca9e5b3e91ff47d964b4fb5553bb9
mfdzerohour <mfdzerohour@hotmail.com>
2025-05-09 08:16:34 -0400

    Alterações usando o storage

--- CursoReactParaCompletos2024/src/App.jsx
+++ CursoReactParaCompletos2024/src/App.jsx
@@ -1,38 +1,39 @@
 import AddTask from './components/AddTask/AddTask.jsx';
 import Tasks from './components/Tasks/Tasks.jsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { v4 } from 'uuid';
 
-function App(){
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: 'Estudar programação',
-      description: "Estudar programação para se tornar um desenvolvedor fullstack",
-      isCompleted: false,
-    }, {
-      id: 2,
-      title: 'Estudar inglês',
-      description: "Estudar inglês para se tornar fluente",
-      isCompleted: false,
-    }, {
-      id: 3,
-      title: 'Estudar matemática',
-      description: "Estudar matemática para se tornar um desenvolvedor fullstack",
-      isCompleted: false    
-    },
-  ]);
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || [
+      {
+        id: 1,
+        title: 'Estudar programação',
+        description: "Estudar programação para se tornar um desenvolvedor fullstack",
+        isCompleted: false,
+      },
+      {
+        id: 2,
+        title: 'Estudar inglês',
+        description: "Estudar inglês para se tornar fluente",
+        isCompleted: false,
+      },
+      {
+        id: 3,
+        title: 'Estudar matemática',
+        description: "Estudar matemática para se tornar um desenvolvedor fullstack",
+        isCompleted: false,
+      },
+    ]
+  );
 
-  function onTaskClick(taskId){
-    const newTasks = tasks.map(task => {
-      //PRECISO ATUALIZAR ESSA TAREFA
-      if(task.id === taskId){
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
         return {
           ...task,
           isCompleted: !task.isCompleted,
-        }
-
-        //NÃO PRECISO ATUALIZAR ESSA TAREFA
+        };
       } else {
         return task;
       }
@@ -41,12 +42,23 @@ function App(){
     setTasks(newTasks);
   }
 
-  function onTaskDeleteClick(taskId){
-    const newTasks = tasks.filter(task => task.id !== taskId);
+  useEffect(() => {
+    if (tasks) {
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }, [tasks]);
+
+  function onTaskDeleteClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
   }
 
-  function onAddTaskSubmit(title, description){
+  function onAddTaskSubmit(title, description) {
+    if (!title.trim() || !description.trim()) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+
     const newTask = {
       id: v4(),
       title,
@@ -67,7 +79,7 @@ function App(){
         <Tasks tasks={tasks} onTaskClick={onTaskClick} onTaskDeleteClick={onTaskDeleteClick} />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
